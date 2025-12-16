c66c3f8492ba580f62e4bf6374c88fed8bf8513f
devbacon <devon.bacon@gmail.com>
2025-12-15 19:19:42 -0500

    add get_book_words func to retrieve word count

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ def get_book_text(book_path):
     with open(book_path) as file:
         return file.read()
     
+def get_book_words(book_path):
+    with open(book_path) as file:
+        return file.read().split()
+    
 def main():
     book_text = get_book_text(book_path)
     print(f"Book length: {len(book_text)} characters")
-    print(book_text)
+
+    book_words = get_book_words(book_path)
+    print(f"Found {len(book_words)} total words")
 
 if __name__ == "__main__":
     main()
